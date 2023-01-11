18b6d576bdc7ea7ac3081d5ff3350f5e638af630
jangdave <jangdave@naver.com>
2023-01-11 15:22:12 +0900

    Initial commit

---
+++ Source/RedDeadRedemption/RedDeadRedemptionGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RedDeadRedemptionGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REDDEADREDEMPTION_API ARedDeadRedemptionGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
